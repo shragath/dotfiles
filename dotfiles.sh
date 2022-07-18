e13cb61ff59b6c94d3127976da099f91355c4c67
Miguel Parra <shragath@gmail.com>
2022-07-18 11:15:32 -0500

    Change autoinstall script

--- dotfiles.sh
+++ dotfiles.sh
@@ -10,16 +10,12 @@ if ! [ -x "$(command -v git)"]; then
     sudo add-apt-repository ppa:git-core/ppa
     sudo apt update
     sudo apt install git
-fi;
-
+fi
 
 if ! [[ -f "$DOTFILES_DIR/.zshrc" ]]; then
     echo "Installing dotfiles";
     git clone --bare https://github.com/shragath/dotfiles $HOME/.dotfiles
 
-    function config {
-       /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
-    }
 
     mkdir -p .config-backup
     config checkout
@@ -29,13 +25,15 @@ if ! [[ -f "$DOTFILES_DIR/.zshrc" ]]; then
     else
         echo "Backing up pre-existing dot files.";
         config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-    fi;
+    fi
 
     config checkout
     config config status.showUntrackedFiles no
-fi;
+fi
 
-
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
 
 if ! [ -x "$(command -v nvim)" ]; then
     echo "Installing nvim";
@@ -44,14 +42,14 @@ if ! [ -x "$(command -v nvim)" ]; then
     git clone https://github.com/neovim/neovim
     cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
     sudo make install
-fi;
+fi
 
 if ! [ -x "$(command -v kitty)" ]; then
     echo "Installing kitty";
     curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-fi;
+fi
 
 if ! [ -x "$(command -v zsh)" ]; then
     echo "Installing zsh";
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi;
+fi
